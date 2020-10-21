1b2f25e9b6c08ad4e1221f346705bdbb9b869928
ethan <izawaryosuke0101@gmail.com>
2020-10-21 12:21:34 +0900

    date

--- date/main.js
+++ date/main.js
@@ -6,5 +6,13 @@ const date = now.getDate();
 const hour = now.getHours();
 const min = now.getMinutes();
 
-const output = `${year}/${month+1}/${date} ${hour}:${min} `;
+let ampm = '';
+if(hour < 12){
+  ampm = 'a.m';
+} else {
+  ampm = 'p.m';
+}
+
+const output = `${year}/${month+1}/${date} ${hour%12}:${min}${ampm} `;
 document.getElementById('time').textContent = output;
+
